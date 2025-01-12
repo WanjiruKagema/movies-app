67762cb334e00fc7a633ff47bae25134be33bc98
WanjiruKagema <38919449+WanjiruKagema@users.noreply.github.com>
2025-01-12 17:45:11 +0300

    chores:updated mongodb file

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'm.media-amazon.com',
-            pathname: '/images/**',
-           
-          },
-          {
-            protocol: 'https',
-            hostname: 'www.postergully.com',
-            pathname: '/cdn/shop/products/**',
-           
-          },
-          {
-            protocol: 'https',
-            hostname: 'images-na.ssl-images-amazon.com',
-            pathname: '/images/I/**',
-           
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        pathname: '/images/**',
+
       },
+      {
+        protocol: 'https',
+        hostname: 'www.postergully.com',
+        pathname: '/cdn/shop/products/**',
+
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+        pathname: '/images/I/**',
+
+      },
+    ],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
