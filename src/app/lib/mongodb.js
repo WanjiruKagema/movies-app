5cccaa4ceb97463965ab7d6d05fab30defb17790
WanjiruKagema <38919449+WanjiruKagema@users.noreply.github.com>
2025-01-12 17:34:25 +0300

    chores:updated mongodb file

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,9 +1,5 @@
 import { MongoClient } from 'mongodb'
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local')
-}
-
 const uri = process.env.MONGODB_URI
 const options = {
   maxPoolSize: 10,
